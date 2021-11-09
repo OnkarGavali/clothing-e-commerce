3fe2a242ea876dc5c9bc0cc875d5b080b7f74b20
“OnkarGavali” <onkargavali1561@gmail.com>
2021-11-09 21:33:05 +0530

    converted hardcoded userReducer action type into seperate object

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,6 @@
+import { UserActionsType } from "./user.types";
+
 export const setCurrentUser = user => ({
-    type : 'SET_CURRENT_USER',
+    type : UserActionsType.SET_CURRENT_USER,
     payload : user
 });
---
+++ src/redux/user/user.types.js
@@ -0,0 +1,3 @@
+export const UserActionsType = {
+    SET_CURRENT_USER : "SET_CURRENT_USER"
+}
