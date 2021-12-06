4fcd01ebbcd2ce0bd282a0e4aa4cd60ef9be6747
“OnkarGavali” <onkargavali1561@gmail.com>
2021-12-06 15:37:59 +0530

    cart-icon updated

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,17 +5,23 @@ import './cart-icon.styles.scss'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden,itemCount }) => {
     return (
         <div className='cart-icon' onClick = {toggleCartHidden}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{itemCount}</span>
         </div>
     )
 }
+
+const mapStateToProps = ({ cart: {cartItems}}) => ({
+    itemCount : cartItems.reduce(
+        (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,0 
+    )
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden : () => dispatch(toggleCartHidden())
 })
 
-export default connect( null, mapDispatchToProps)( CartIcon );
+export default connect( mapStateToProps, mapDispatchToProps)( CartIcon );
